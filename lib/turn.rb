d5cf4e999b5efeeb903ea57c45589d21596ec524
Brian Palmer <bpalmer710@gmail.com>
2020-10-29 19:07:44 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board,index)
+  end
+
+  def move
+
+  end
